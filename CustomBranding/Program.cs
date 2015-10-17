7735b3ac13a7fbb3a7f0e599b9649b8ec51abf54
spprashaant <thesharepointguide@gmail.com>
2015-10-17 16:41:55 +0530

    Project Files
    Files

---
+++ CustomBranding/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+
+namespace CustomBranding
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Uri siteUri = new Uri("https://your-domain.sharepoint.com");
+    string realm = TokenHelper.GetRealmFromTargetUrl(siteUri);
+ 
+    string accessToken = TokenHelper.GetAppOnlyAccessToken(TokenHelper.SharePointPrincipal,
+                                                            siteUri.Authority, realm).AccessToken;
+ 
+    using (var clientContext = TokenHelper.GetClientContextWithAccessToken(siteUri.ToString(), accessToken))
+    {
+      Web web = clientContext.Web;
+      web.UploadThemeFile("../../custom.spcolor");
+      web.UploadThemeFile("../../Background.png");
+
+      clientContext.Load(web, w => w.AllProperties, w => w.ServerRelativeUrl);
+      clientContext.ExecuteQuery();
+      // Let's first upload the custom theme to host web
+      web.CreateComposedLookByName("customTheme",
+                      web.ServerRelativeUrl + "/_catalogs/theme/15/custom.spcolor",
+                      null,
+                      clientContext.Web.ServerRelativeUrl + "/_catalogs/theme/15/Background.png",
+                      string.Empty);
+      clientContext.ExecuteQuery();
+      // Setting the custom theme to host web
+      web.SetComposedLookByUrl("customTheme");
+      //web.SetComposedLookByUrl("Office");
+    }
+ 
+    Console.WriteLine("...");
+    Console.ReadLine();
+    }
+  }
+}
